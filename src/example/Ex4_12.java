fc44d40a73db211cd85f2dc0d919e99239b60dce
ryudb0 <ryudb030@gmail.com>
2023-01-25 21:13:11 +0900

    연습문제 4_12 답봄..

--- src/example/Ex4_12.java
+++ src/example/Ex4_12.java
@@ -5,13 +5,14 @@ public class Ex4_12 {
 //		이거진짜 모르겠어
 		for(int i=1; i<=9; i++) {
 			for(int j=1; j<=3; j++) {
-				int x = 0;		// 단
+				int x = j+1 +(i-1)/3*3;		// 단
 				int y = i%3==0 ? 3 : i%3;		// 곱하는수
 				
-				System.out.println(x+"*"+y+"="+x*y);
+				System.out.print(x+"*"+y+"="+x*y+" ");
 				
 				
 			}
+			System.out.println();
 		}
 		
 		
